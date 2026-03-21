576d0e760566727916b38406a359825bf9a717c5
Calderon1711 <alejandro.calderon.campos@est.una.ac.cr>
2026-03-21 15:49:02 -0600

    repository listo

---
+++ src/main/java/org/example/bolsa_empleo/repository/CvRepository.java
@@ -0,0 +1,9 @@
+package org.example.bolsa_empleo.repository;
+
+import org.example.bolsa_empleo.entidades.CV;
+import org.springframework.data.jpa.repository.JpaRepository;//importa la interfaz de spring data jpa(inclutye metiodos como guardar. buscarx id,etc)
+import org.springframework.stereotype.Repository;//esto le dice a spring que esta interfaz es un componente de acceso de datos
+
+@Repository
+public interface CvRepository extends JpaRepository<CV, Long> { //define la interfaz que va manejar la tabla de empresa
+}
